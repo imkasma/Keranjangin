61149ac334b22c15eb79506c064210b7445b3644
palupiazalia <lupiktp@gmail.com>
2026-04-30 13:14:59 +0700

    empty cart page

--- lib/views/cart/empty_cart_page.dart
+++ lib/views/cart/empty_cart_page.dart
@@ -1,3 +1,4 @@
+// views/cart/empty_cart_page.dart
 import 'package:flutter/material.dart';
 
 import '../../core/components/network_image.dart';
@@ -8,36 +9,40 @@ class EmptyCartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: SizedBox(
+        width: size.width,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Spacer(flex: 2),
           SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7,
+              width: size.width * 0.7,
             child: const Padding(
               padding: EdgeInsets.all(AppDefaults.padding * 2),
               child: AspectRatio(
-                aspectRatio: 1 / 1,
-                child:
-                    NetworkImageWithLoader('https://i.imgur.com/3avdket.png'),
+                  aspectRatio: 1,
+                  child: NetworkImageWithLoader(
+                    'https://i.imgur.com/3avdket.png',
+                  ),
+                ),
               ),
             ),
-          ),
-          Text(
-            'Oppss!',
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge
-                ?.copyWith(fontWeight: FontWeight.bold, color: Colors.black),
+            Text(
+              'Oppss!',
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
           ),
           const SizedBox(height: 8),
           const Text('Sorry, you have no products in your cart'),
           const Spacer(),
-          SizedBox(
-            width: double.infinity,
-            child: Padding(
+
+            /// 🔘 Button
+            Padding(
               padding: const EdgeInsets.all(AppDefaults.padding * 2),
               child: ElevatedButton(
                 onPressed: () {},
